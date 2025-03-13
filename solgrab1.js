ffc57ce8ca14f59190a7a06f39d79da4b45bdb8d
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2025-03-13 09:51:06 -0300

    Update and rename solgrab1.js to solgrab1js

--- solgrab1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-$('#element_2').change(function(){ 
-
-if ($('#element_2').val().length < 2 || $('#element_2').val().substr(0,2) != "rt"  ) { $('#element_2').val(""); }
-
-});
-
-});
