87c5232980a747d32a4757e87cc99d5f96f9743f
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2025-03-26 20:35:54 -0300

    Update and rename otrasgestiones_2.4.6.js to otrasgestiones_2.4.7.js

--- otrasgestiones_2.4.7.js
+++ otrasgestiones_2.4.7.js
@@ -112,8 +112,8 @@ function actualizarFechas() {
     let nocheCantidad = noches.value;
     if (nocheCantidad) resultado += `- Corresponde ${nocheCantidad} ciclo/s de 24hs.\n`;
 
-    let fecha_salida_date = new Date(`${fecha_salida}T${hora_inicio}:00`);
-    let fecha_fin_jornada = new Date(`${fecha_salida}T${hora_fin}:00`);
+    let fecha_salida_date = new Date(fecha_salida.replace(' ', 'T'));
+    let fecha_fin_jornada = new Date(${fecha_salida_date.toISOString().split('T')[0]}T${hora_fin}:00);
 
     if (fecha_salida_date <= fecha_fin_jornada) {
       if (!estaEnHorarioLaboral(fecha_salida, hora_inicio, hora_fin)) {
