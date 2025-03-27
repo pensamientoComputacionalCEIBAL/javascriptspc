184cde7839aa57def734a2755272babcc18b8a55
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2025-03-27 20:44:45 -0300

    Create encuesta_primerciclo.1.js

---
+++ encuesta_primerciclo.1.js
@@ -0,0 +1,5 @@
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.location.href.indexOf("page=1") !== -1) { window.location.href = window.location.href.replace("page=1", "page=2"); }
+});
+</script>
